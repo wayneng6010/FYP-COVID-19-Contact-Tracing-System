d307bc7ccd5bd6895c2f08950da59b96c90303b0
wayneng6010 <41977079+wayneng6010@users.noreply.github.com>
2020-07-26 00:41:23 +0800

    Initial commit

---
+++ Artist.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const db =
+	"mongodb+srv://dbUser:627233@clustertesting-j5vap.mongodb.net/Artist?retryWrites=true&w=majority";
+const path = require("path");
+
+//Connect to MongoDB database
+mongoose
+	.connect(process.env.MONGODB_URI || db, { useNewUrlParser: true })
+	.then(() => {
+		console.log("Connected to database");
+	})
+	.catch(() => {
+		console.log("Error Connected to database");
+	});
+
+// artist schema
+const artistSchema = new mongoose.Schema({
+	ID: { type: Number },
+	Name: { type: String },
+	PictureURL: { type: String },
+	AlbumNum: { type: Number },
+	FansNum: { type: Number },
+	UserID: { type: String }, //to identify saved artist of each user
+});
+
+// user schema
+const userSchema = new mongoose.Schema({
+	name: { type: String },
+	email: { type: String },
+	password: { type: String },
+	date: { type: Date, default: Date.now },
+});
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const Artist = mongoose.model("Data", artistSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports.Artist = Artist;
+module.exports.User = User;
---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const Artist = require("./Artist").Artist;
+const User = require("./Artist").User;
+var path = require("path");
+// google vision api
+// const vision = require("@google-cloud/vision");
+// const client = new vision.ImageAnnotatorClient({
+// 	keyFilename: "my-key.json",
+// });
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.post("/ic_ocr", async (req, res) => {
+	// const fileName = './picture-730550.jpg';
+	// console.log("filename:");
+	// axios
+	// 	.post("https://vision.googleapis.com/v1/images:annotate", {
+	// 		body: req.body,
+	// 	})
+	// 	.then((response) => {
+	// 		let responseJson = response.json();
+	// 		console.log(responseJson);
+	// 		// res.send(response.data);
+	// 	})
+	// 	.catch((error) => {
+	// 		res.status(400).json(error);
+	// 	});
+	// let responseJson = await response.json();
+	// console.log(responseJson);
+	// try {
+	// 	const [result] = await client.textDetection(req.body.user.ic_uri);
+	// 	const detections = result.textAnnotations;
+	// 	console.log("Text:");
+	// 	detections.forEach((text) => console.log(text));
+	// 	res.send(true);
+	// } catch (error) {
+	// 	console.log(error);
+	// 	res.status(400).json(error);
+	// }
+
+	// const features = [{ type: "TEXT_DETECTION" }];
+
+	// // Build the image request object for that one image. Note: for additional images you have to create
+	// // additional image request objects and store them in a list to be used below.
+	// const imageRequest = {
+	// 	image: {
+	// 		content: req.body.user.ic_base64
+	// 	},
+	// 	features: features,
+	// };
+
+	// // Set where to store the results for the images that will be annotated.
+	// const outputConfig = {
+	// 	gcsDestination: {
+	// 		uri: outputUri,
+	// 	},
+	// 	batchSize: 2, // The max number of responses to output in each JSON file
+	// };
+
+	// // Add each image request object to the batch request and add the output config.
+	// const request = {
+	// 	requests: [
+	// 		imageRequest, // add additional request objects here
+	// 	],
+	// 	outputConfig,
+	// };
+
+	// // Make the asynchronous batch request.
+	// const [operation] = await client.asyncBatchAnnotateImages(request);
+
+	// // Wait for the operation to complete
+	// const [filesResponse] = await operation.promise();
+
+	// console.log(filesResponse);
+	// // The output is written to GCS with the provided output_uri as prefix
+	// // const destinationUri = filesResponse.outputConfig.gcsDestination.uri;
+	// // console.log(`Output written to GCS with prefix: ${destinationUri}`);
+});
+
+// app.post("/register", async (req, res) => {
+// 	const user = new User({
+// 		name: req.body.user.name,
+// 		email: req.body.user.email,
+// 		password: req.body.user.psw,
+// 	});
+// 	try {
+// 		const savedUser = await user.save();
+// 		res.send(true);
+// 	} catch (error) {
+// 		res.status(400).json(error);
+// 	}
+// });
+
+// app.get("/getArtistRelatedNews", (req, res) => {
+// 	const artist_name = req.query.artist_name;
+// 	const apikey_news = "fbeba8d45b5c49e88f83dbb9b40cbe48";
+
+// 	const querystr = `https://newsapi.org/v2/everything?q="${artist_name}"&apiKey=${apikey_news}`;
+
+// 	axios
+// 		.get(querystr)
+// 		.then((response) => {
+// 			res.send(response.data);
+// 		})
+// 		.catch((error) => {
+// 			res.status(400).json(error);
+// 		});
+// });
+
+// heroku
+if (process.env.NODE_ENV === "production") {
+	// Exprees will serve up production assets
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
